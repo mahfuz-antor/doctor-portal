672253680e7fee0512f359a1298af17e42d9bfe1
mahfuz-antor <antorcnj2014@gmail.com>
2022-12-21 16:18:02 +0600

    banner part done

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Banner from "./Banner";
 
 const Home = () => {
   return (
-    <div>
-      <h2> Home Page </h2>
-    </div>
+    <>
+      <Banner />
+    </>
   );
 };
 
